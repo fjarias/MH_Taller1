04cff42d5a5794dfbe9516d94d9a51383c8d50ea
FranciscoJavierArias <franciscojavierarias@gmail.com>
2020-07-11 09:01:15 -0500

    Commit Versión inicial para Ments

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -4,24 +4,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
-
+public class FirstFragment extends Fragment implements AdapterView.OnItemSelectedListener
+{
+    static final String[] postres = new String[] {"A","B","C"};
+    static final String[] precios = new String[] {"10","20","30"};
+    TextView tv;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        tv = view.findViewById(R.id.textview_first);
+        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                tv.setText("Hola mundo");
+            }
+        });
+
 
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +47,15 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
