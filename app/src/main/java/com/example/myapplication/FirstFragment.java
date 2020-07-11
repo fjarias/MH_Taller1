363768c692f03c6b70f5242e15cac514970b3e4d
FranciscoJavierArias <franciscojavierarias@gmail.com>
2020-07-11 12:03:51 -0500

    Activity primer sin paso de parametros entre activities

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,8 +14,14 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment implements AdapterView.OnItemSelectedListener
 {
-    static final String[] postres = new String[] {"A","B","C"};
-    static final String[] precios = new String[] {"10","20","30"};
+    static final String[] orTime = new String[] {"< 30","31-60","61-120","121-180",">181"};
+    static final String[] losAnticipated = new String[] {"out patient","23 hrs","24-48 hrs","< 3d",">4d"};
+    static final String[] postOpICU = new String[] {"very unlikely","< 5%","5%-10%","11%-25%","> 25%"};
+    static final String[] bleedingRisk = new String[] {"< 100","101-250","251-500","501-750","> 750"};
+    static final String[] surgicalSize = new String[] {"1","2","3","4","> 4"};
+    static final String[] intubation = new String[] {"< 1%","1%-5%","6%-10%","11%-25%","> 25%"};
+    static final String[] surgicalSite = new String[] {"none of the following","adbominopelvic MIS surgery","abdominopelvic Open Surgery (infraumbilical)","abdominopelvic Open Surgery (supraumbilical)","OHNS/Upper GI/Thoracic"};
+    Spinner spORTime;
     TextView tv;
     @Override
     public View onCreateView(
@@ -23,10 +30,39 @@ public class FirstFragment extends Fragment implements AdapterView.OnItemSelecte
     ) {
         // Inflate the layout for this fragment
 
-
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
+/*
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_first);
+        textoBebidas = findViewById(R.id.textValor1);
+        textoPostres = findViewById(R.id.textPostres);
 
+        //Asigna el spinner del XML a la variable spinner
+        spinnerBebidas = findViewById(R.id.spinnerBebidas);
+        //Crea un adaptador para el spinner, que va a meterle los strings que se encuentran en el arreglo "options"
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, BEBIDAS);
+        //Agrega el adaptador al spinner, para que muestre todos los items de la lista "options"
+        spinnerBebidas.setAdapter(adapter);
+        //Selecciona esta activity como quien escucha cuando se selecciona un item
+        spinnerBebidas.setOnItemSelectedListener(this);
+
+        //Spinner Postres
+        spinnerPostres = findViewById(R.id.spinnerPostre);
+        //adaptador
+        ArrayAdapter<String> adapterPostres = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, POSTRES);
+        //Asignar adaptador al spinner postres
+        spinnerPostres.setAdapter(adapterPostres);
+        //Seleccionar esto como el listener
+        spinnerPostres.setOnItemSelectedListener(this);
+
+    }
+
+    private void setContentView(int fragment_first) {
+    }
+*/
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
---
+++ app/src/main/java/com/example/myapplication/PrimerActivity.java
@@ -0,0 +1,79 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class PrimerActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    int[] valoresSpinners;
+
+    static final String[] orTime = new String[] {"< 30","31-60","61-120","121-180",">181"};
+    static final String[] losAnticipated = new String[] {"out patient","23 hrs","24-48 hrs","< 3d",">4d"};
+    static final String[] postOpICU = new String[] {"very unlikely","< 5%","5%-10%","11%-25%","> 25%"};
+    static final String[] bleedingRisk = new String[] {"< 100","101-250","251-500","501-750","> 750"};
+    static final String[] surgicalSize = new String[] {"1","2","3","4","> 4"};
+    static final String[] intubation = new String[] {"< 1%","1%-5%","6%-10%","11%-25%","> 25%"};
+    static final String[] surgicalSite = new String[] {"none of the following","adbominopelvic MIS surgery","abdominopelvic Open Surgery (infraumbilical)","abdominopelvic Open Surgery (supraumbilical)","OHNS/Upper GI/Thoracic"};
+    Spinner spORTime;
+    Spinner spLosAnticipated;
+    TextView tvMents;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primer);
+
+        valoresSpinners = new int[7];
+        int i;
+        for (i = 0;i < 7;i++) {
+            valoresSpinners[i] = 0;
+        }
+
+        tvMents = findViewById(R.id.tvMents);
+        spORTime = findViewById(R.id.spORTime);
+        spLosAnticipated = findViewById(R.id.spLosAnticipated);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, orTime);
+        spORTime.setAdapter(adapter);
+        spORTime.setOnItemSelectedListener(this);
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, losAnticipated);
+        spLosAnticipated.setAdapter(adapter1);
+        spLosAnticipated.setOnItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        switch (parent.getId()) {
+            case R.id.spORTime: //Manejar el spinner 1
+                valoresSpinners[0] = pos + 1;
+                break;
+            case R.id.spLosAnticipated:
+                valoresSpinners[1] = pos + 1;
+                break;
+        }
+        sumarParametros();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    public void sumarParametros(){
+        int i, total;
+        total = 0;
+        for (i = 0;i < 7;i++){
+            total += valoresSpinners[i];
+        }
+        tvMents.setText(Integer.toString(total));
+    }
+}
